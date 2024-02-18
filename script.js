c23d8bd84ed2965ea73330ec7c90194f1e9feb25
Protheu-42 <everaldo.sobrinho42@gmail.com>
2024-02-18 11:57:12 -0300

    playerChoose add

---
+++ script.js
@@ -0,0 +1,33 @@
+// Create variable to hold all choices
+const options = ["rock", "paper", "scissors"];
+
+// Player input
+let playerChoose = prompt("Rock, paper or scissors?");
+
+// Put input to lowercase
+playerChoose = playerChoose.toLowerCase();
+
+// Check if the answer is one of the three options
+if (playerChoose == "rock" || playerChoose == "paper" || playerChoose == "scissors"){
+    console.log("Very Good");
+}
+else {
+    console.log("Choose one of the three options!")
+}
+
+// Computer input
+let computerChoose = getComputerChoose();
+
+    // Function for the computer choose one of three options
+function getComputerChoose() {
+    const random = Math.floor(Math.random() * options.length);
+    return options[random];
+};
+
+        // Save the result in a variable
+
+// Function to compare results
+    // Comparison through simple if or else
+    // Return result
+
+// Display the winner
